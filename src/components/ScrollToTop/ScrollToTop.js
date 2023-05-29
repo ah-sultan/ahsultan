fc11801f6b28c60d303d96fa9f201a19f707511a
Sultan Ahmed <128618070+ah-sultan@users.noreply.github.com>
2023-05-29 12:15:55 +0600

    Update ScrollToTop.js

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,39 +1,44 @@
-import React, { useEffect, useState } from 'react';
+
+
+import React, { useState, useEffect } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 
-const ScrollToTopButton = () => {
-  const [showButton, setShowButton] = useState(false);
 
-  const handleScroll = () => {
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
     if (window.pageYOffset > 300) {
-      setShowButton(true);
+      setIsVisible(true);
     } else {
-      setShowButton(false);
+      setIsVisible(false);
     }
   };
 
   const scrollToTop = () => {
-    const scrollStep = -window.scrollY / 5;
-    const scrollInterval = setInterval(() => {
-      if (window.scrollY !== 0) {
-        window.scrollBy(0, scrollStep);
-      } else {
-        clearInterval(scrollInterval);
-      }
-    }, 5);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   };
 
- useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
- })
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
 
   return (
     <>
-       <button className={`w-7 h-7  outline outline-1 outline-primary outline-offset-4 rounded-full center-child fixed z-50 right-4 bottom-8  trns-1 bg-primary shadow-dots ${showButton ? 'translate-y-0 visible' : 'translate-y-9 invisible'}`} onClick={scrollToTop}>
+      
+        <button className={`w-7 h-7  outline outline-1 outline-primary outline-offset-4 rounded-full center-child fixed z-50 right-4 bottom-8  trns-1 bg-primary shadow-dots ${isVisible ? 'translate-y-0 visible' : 'translate-y-9 invisible'}`} onClick={scrollToTop}>
           <FaArrowUp />
         </button>
+      
     </>
   );
 };
 
-export default ScrollToTopButton;
+export default ScrollToTop;
+
