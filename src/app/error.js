969a116a79e0002d3598be569e55ff559c710962
ah-sultan <ah.sultan.dev@gmail.com>
2024-05-28 22:48:36 +0600

    DONE

--- src/app/error.js
+++ src/app/error.js
@@ -1,10 +1,18 @@
 "use client";
-const error = ({ error, reset }) => {
+import { useEffect } from "react";
+const Error = ({ error, reset }) => {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
     <>
-      <div className="w-100 d-flex align-items-center justify-content-center" style={{height: "100vh"}}>
+      <div
+        className="w-100 d-flex align-items-center justify-content-center"
+        style={{ height: "100vh" }}
+      >
         <h5 className="text-center mt-4 pt-4">
-          Something went wrong <br /><br />
+          Something went wrong <br />
+          <br />
           <button type="button" className="theme-btn" onClick={() => reset()}>
             Try Again
           </button>
@@ -14,4 +22,4 @@ const error = ({ error, reset }) => {
   );
 };
 
-export default error;
+export default Error;
