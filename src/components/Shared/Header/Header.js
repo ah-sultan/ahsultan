aab186986ea24c73a7b59fe8a2424f25f0c8ec5a
ah-sultan <ah.sultan.dev@gmail.com>
2024-05-29 23:16:29 +0600

    Update

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -39,7 +39,7 @@ const Header = ({ onePageMenu }) => {
                 <div className="navbar-header">
                   <div className="mobile-logo my-15">
                     <Link href="/">
-                      <Logo type="light"/>
+                      <Logo type="light" />
                     </Link>
                   </div>
                   {/* Toggle Button */}
@@ -50,9 +50,19 @@ const Header = ({ onePageMenu }) => {
                     data-bs-target=".navbar-collapse"
                     onClick={() => setToggle(!toggle)}
                   >
-                    <span className="icon-bar" />
-                    <span className="icon-bar" />
-                    <span className="icon-bar" />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#000"
+                      version="1.1"
+                      viewBox="0 0 290 290"
+                      xmlSpace="preserve"
+                      style={{ width: 40, height: 40 }}
+                    >
+                      <path d="M165 0H290V125H165z"></path>
+                      <path d="M0 0H125V125H0z"></path>
+                      <path d="M0 165H125V290H0z"></path>
+                      <path d="M165 165H290V290H165z"></path>
+                    </svg>
                   </button>
                 </div>
                 <div
